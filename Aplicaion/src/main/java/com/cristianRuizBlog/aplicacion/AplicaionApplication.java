e80ec9d77bf16bfc8c820f8672e2d2fca09437b2
cruizg93 <crisruiz93@gmail.com>
2019-05-07 19:58:38 -0500

    Comit inicial con eclipse

---
+++ Aplicaion/src/main/java/com/cristianRuizBlog/aplicacion/AplicaionApplication.java
@@ -0,0 +1,13 @@
+package com.cristianRuizBlog.aplicacion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AplicaionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AplicaionApplication.class, args);
+	}
+
+}
